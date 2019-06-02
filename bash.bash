ce294877580bd1b0330e2904dd081198ebe49201
dsdan7 <40010858+dsdan7@users.noreply.github.com>
2019-06-02 11:55:52 -0300

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -1,4 +1,4 @@
-command=$(curl -XPOST https://www.facebook.com/recover/code/?u=100015745421328&n=046370&s=23&exp_locale=en_US&redirect_from=button/)
+command=$(curl -XPOST https://www.yourlink.com/)
 echo ${#command}   # is to take the lenght of the result of cURL 
 var=${#command};  
 destdir=/root/txt.txt
